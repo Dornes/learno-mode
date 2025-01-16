f32ac1ffeab924264f7b7640aed4af88144fe84f
Sondre Stokke <69515086+Dornes@users.noreply.github.com>
2025-01-16 14:09:07 +0100

    Fix assignment card styling (#18)
    Co-authored-by: Sebastian <sebheg77@gmail.com>

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,28 +24,34 @@ export default function Index() {
 
   return (
     <>
-      <div className="w-4/5 md:w-3/5 2xl:w-2/5 mx-auto mt-10">
+      <div className="w-4/5 md:w-3/5 mx-auto mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
           {assignments.map((assignment: Assignment) => (
             <div key={assignment.id}>
               <Link to={`/assignment.${assignment.id}`}>
-                <Card className="items-center hover:bg-gray-50">
+                <Card className="items-center hover:bg-gray-50 h-44 overflow-hidden">
                   <CardHeader>
                     <div className="flex flex-row space-x-2">
-                      <CardTitle>{assignment.title}</CardTitle>
+                      <CardTitle className="truncate">
+                        {assignment.title}
+                      </CardTitle>
                       <CircleCheckBig
                         className={clsx("w-6 h-6 flex-shrink-0 flex-grow-0", {
                           "text-green-500": assignment.all_tasks_approved,
                         })}
                       />
                     </div>
-                    <CardDescription>{assignment.description}</CardDescription>
+                    <CardDescription className="truncate">
+                      {assignment.description}
+                    </CardDescription>
                   </CardHeader>
-                  <CardContent>
+                  <CardContent className="flex flex-col justify-center">
                     <Progress
                       value={
-                        100 *
-                        (assignment.approved_tasks / assignment.total_tasks)
+                        assignment.total_tasks !== 0
+                          ? 100 *
+                            (assignment.approved_tasks / assignment.total_tasks)
+                          : 0
                       }
                     />
                     <p className="mt-1 font-extralight">
