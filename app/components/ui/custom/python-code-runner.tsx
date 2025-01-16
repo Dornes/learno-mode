4b6501b8bad8fa55e5dbcf7ef1438c81e2312bec
Sebastian <sebheg77@gmail.com>
2025-01-16 11:40:11 +0100

    Working code run example

--- app/components/ui/custom/python-code-runner.tsx
+++ app/components/ui/custom/python-code-runner.tsx
@@ -1,27 +1,69 @@
 import { loadPyodide, PyodideInterface } from "pyodide";
 import { useEffect, useState } from "react";
+import { Textarea } from "../textarea";
+import { Button } from "../button";
 
 function PythonCodeRunner() {
   const [pyodide, setPyodide] = useState<PyodideInterface | null>(null);
+  const [output, setOutput] = useState<string | null>(null);
+  const [codeInput, setCodeInput] = useState<string>("");
+  const [printOutput, setPrintOutput] = useState<string | null>(null);
 
-  useEffect(() => {
+  //   useEffect(() => {
+  //     if (typeof window !== "undefined") {
+  //       loadPyodide({
+  //         indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.1/full",
+  //         stderr: (text) => console.log(text),
+  //         stdout: (text) => console.log(text),
+  //       })
+  //         .then((result) => {
+  //           setPyodide(result);
+  //           result.runPython("print(2+2)");
+  //         })
+  //         .catch((error) => {
+  //           console.error("Error loading Pyodide:", error);
+  //         });
+  //     }
+  //   }, []);
+
+  const runCode = async () => {
     if (typeof window !== "undefined") {
       loadPyodide({
         indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.1/full",
-        stderr: (text) => console.log(text),
-        stdout: (text) => console.log(text),
+        stderr: (text) => setPrintOutput(text),
+        stdout: (text) => setPrintOutput(text),
       })
         .then((result) => {
           setPyodide(result);
-          result.runPython("print(2+2)");
+          const output = result.runPython(codeInput);
+          setOutput(output);
+          console.log(output);
+          console.log(printOutput);
         })
         .catch((error) => {
           console.error("Error loading Pyodide:", error);
         });
     }
-  }, []);
+  };
 
-  return <div>Pyodide Loaded: {pyodide ? "Yes" : "No"}</div>;
+  return (
+    <div>
+      Pyodide Loaded: {pyodide ? "Yes" : "No"}
+      <Textarea
+        placeholder="Enter Python code here"
+        value={codeInput}
+        onChange={(e) => setCodeInput(e.target.value)}
+      />
+      <Button onClick={() => runCode()}>Run Code</Button>
+      {output !== null && (
+        <div>
+          <h2>Output:</h2>
+          <pre>{output}</pre>
+          <pre>{printOutput}</pre>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PythonCodeRunner;
