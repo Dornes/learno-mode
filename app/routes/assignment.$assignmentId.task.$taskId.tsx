d829ff4d01d2f11e1df1e57862e8abb5f49df650
Sondre Stokke <sondre7stokke@live.no>
2025-02-10 15:04:14 +0100

    Fix double goofup

--- app/routes/assignment.$assignmentId.task.$taskId.tsx
+++ app/routes/assignment.$assignmentId.task.$taskId.tsx
@@ -16,7 +16,7 @@ export default function Task() {
       <h1 className="text-2xl font-bold">{task.title}</h1>
       <p className="mt-4">{task.description}</p>
       <PythonCodeRunner solution={task.solution} test_code={task.test_code} />
-      <ChatButton isEvaluation={false} />
+      <ChatButton />
     </div>
   );
 }
