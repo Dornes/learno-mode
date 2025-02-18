847763511a25d333fdd5f82bf28cf1388e007a2a
Sondre Stokke <sondre7stokke@live.no>
2025-02-18 13:16:33 +0100

    update version in component

--- app/components/custom/python-code-runner.tsx
+++ app/components/custom/python-code-runner.tsx
@@ -28,7 +28,7 @@ function PythonCodeRunner({ solution, test_code }: PythonCodeRunnerProps) {
   useEffect(() => {
     if (typeof window !== "undefined") {
       loadPyodide({
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.1/full",
+        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.2/full",
         stderr: (text) => console.log(text),
         stdout: (text) => printHandler(text),
       })
