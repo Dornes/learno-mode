a5171584200a0a097811244188b27771c0daf712
Sondre Stokke <sondre7stokke@live.no>
2025-03-24 13:43:51 +0100

    remove unused code

--- app/routes/assignment.$assignmentId.tsx
+++ app/routes/assignment.$assignmentId.tsx
@@ -1,11 +1,9 @@
-import { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { LoaderFunction } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import Sidebar from "~/components/custom/sidebar";
 import { assignmentLoader } from "~/services/assignment-loader";
-import { taskAction } from "~/services/task-action";
 
 export const loader: LoaderFunction = assignmentLoader;
-export const action: ActionFunction = taskAction;
 
 export default function Assignment() {
   const { assignment, tasks } = useLoaderData<typeof loader>();
