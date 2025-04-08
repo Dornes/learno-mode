6b2050da11abb40f458d8f73343e8c04d22f989f
Sebastian <sebheg77@gmail.com>
2025-04-08 10:53:07 +0200

    Add back in admin button and move it under the header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,11 +24,11 @@ export default function Index() {
 
   return (
     <>
-      {/* <Link className="fixed bottom-3 left-1/2 text-gray-600" to={"/admin"}>
-        Admin
-      </Link> */}
-      <div className="w-4/5 md:w-3/5 mx-auto mt-20">
-        <h1 className="text-4xl mb-4">Learno mode</h1>
+      <div className="w-4/5 md:w-3/5 mx-auto mt-20 mb-4">
+        <h1 className="text-4xl">Learno mode</h1>
+        <Link className="text-gray-600" to={"/admin"}>
+          Admin
+        </Link>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
           {assignments.map((assignment: Assignment) => (
             <div key={assignment.id}>
